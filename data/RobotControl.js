ba50eb8aa201d3e8af4b82e7ae1a6a23cde4f768
Jeff Malins <jmalins@iolani.org>
2016-11-26 10:51:47 -0800

    refactored robot communication into separate module

---
+++ data/RobotControl.js
@@ -0,0 +1,140 @@
+/**
+ *  RobotControl
+ *
+ *    Implements real-time connection management and command transmission to the
+ *    robot. External interface is as follows:
+ *
+ *    start() : update the robot continuously
+ *    stop() :  stop updating
+ *
+ *    getState() : current state of the control interface
+ *                  STOPPED     not updating
+ *                  CONNECTING  start() was called, attempting first connection
+ *                  CONNECTED   robot is being updated
+ *                  ERROR       the last update failed
+ *    onstatechanged : handler function to be notified on state changes
+ *
+ *    getUpdateRate() : rate of robot updates in Hz
+ *    getLastError() : the error return, or null if no error
+ *
+ *    setPower(left, right) : update the robot motor powers
+ *    getPower() : returns the current motor powers as a { left, right } object
+ **/
+(function() {
+  var _state, _updateRate = 0;
+  var _leftPower = 0, _rightPower = 0;
+
+  // handle state change //
+  function setState(state) {
+    if(state === _state) return;
+    var oldState = _state;
+    _state = state;
+
+    // notify listener //
+    if(typeof RobotControl.onstatechange === 'function') {
+      RobotControl.onstatechange(state, oldState);
+    }
+  }
+
+  // build a packet from current values //
+  function getPacketValue() {
+    return _leftPower + ':' + _rightPower;
+  }
+
+  // send a control packet to the robot   //
+  // takes an (err, value) style callback //
+  function sendUpdate(value, callback) {
+    var xhr = new XMLHttpRequest();
+
+    xhr.open('PUT', '/control', true);
+    xhr.onload = function(e) {
+      var res = { text: xhr.responseText, status: xhr.status };
+      callback((res.status != 200)? res.text: null, res);
+    };
+    xhr.onerror = function(e) {
+      callback(e.type, e);
+    };
+    xhr.timeout   = 1000; // 1 second //
+    xhr.ontimeout = xhr.onerror;
+
+    xhr.send(value);
+  }
+
+  // start the polling loop //
+  function pollRobot() {
+    var pollStart = new Date();
+    sendUpdate(getPacketValue(), function(err, res) {
+      //console.log('packet received', err, res);
+
+      // was the loop terminated? //
+      _lastError = err;
+      if(_state === RobotControl.STOPPED) {
+        _updateRate = 0;
+        return;
+      }
+
+      // handle errors //
+      if(_lastError) {
+        setState(RobotControl.ERROR);
+      } else if(_state !== RobotControl.CONNECTED) {
+        setState(RobotControl.CONNECTED);
+      }
+
+      // poll again //
+      var pollMs = (_state === RobotControl.ERROR)? 1000: 1; // back off if error //
+      setTimeout(pollRobot, pollMs);
+      
+      // compute update rate //
+      var delayMs = new Date().getTime() - pollStart.getTime();
+      if(delayMs > 0) {
+        _updateRate = Math.floor(1000 * 100 / delayMs) / 100;
+      }
+    });
+  }
+
+  window.RobotControl = {
+    start: function() {
+      setState(RobotControl.CONNECTING);
+      pollRobot();
+    },
+    stop: function() {
+      setState(RobotControl.STOPPED);
+    },
+
+    // state management //
+    STOPPED:    'stopped',
+    CONNECTING: 'connecting',
+    CONNECTED:  'connected',
+    ERROR:      'error',
+    getState: function() {
+      return _state;
+    },
+    onstatechange: null,
+    getLastError: function() {
+      return _lastError;
+    },
+    getUpdateRate: function() {
+      return _updateRate;
+    },
+
+    // control motor power //
+    setPower: function(left, right) {
+      _leftPower = (
+        (left >  1023)? 1023:
+        (left < -1023)? -1023:
+        left
+      );
+      _rightPower = (
+        (right >  1023)? 1023:
+        (right < -1023)? -1023:
+        right
+      );
+    },
+    getPower: function() {
+      return { left: _leftPower, right: _rightPower };
+    }
+  };
+
+  // set initial state //
+  _state = RobotControl.STOPPED;
+})();
--- data/touch.js
+++ data/touch.js
@@ -1,6 +1,6 @@
 
 var canvas, c, container;
-var powerL = 0, powerR = 0, updateRate = 0;
+var powerL = 0, powerR = 0;
 
 setupCanvas();
 
@@ -9,58 +9,58 @@ var touchable = ('createTouch' in document);
 var touches   = []; // array of touch vectors
 
 setInterval(draw, 1000/35); 
-sendUpdate();
+RobotControl.start();
 
 if(touchable) {
-	canvas.addEventListener('touchstart', onTouchStart, false);
-	canvas.addEventListener('touchmove',  onTouchMove,  false);
-	canvas.addEventListener('touchend',   onTouchEnd,   false);
-	window.onorientationchange = resetCanvas;  
-	window.onresize = resetCanvas;  
+  canvas.addEventListener('touchstart', onTouchStart, false);
+  canvas.addEventListener('touchmove',  onTouchMove,  false);
+  canvas.addEventListener('touchend',   onTouchEnd,   false);
+  window.onorientationchange = resetCanvas;  
+  window.onresize = resetCanvas;  
 } else {
-	canvas.addEventListener('mousemove', onMouseMove, false);
+  canvas.addEventListener('mousemove', onMouseMove, false);
 }
 
 function resetCanvas(e) {  
- 	// resize the canvas - but remember - this clears the canvas too. 
+  // resize the canvas - but remember - this clears the canvas too. 
   canvas.width  = window.innerWidth; 
-	canvas.height = window.innerHeight;
-	
-	//make sure we scroll to the top left. 
-	window.scrollTo(0, 0); 
+  canvas.height = window.innerHeight;
+  
+  //make sure we scroll to the top left. 
+  window.scrollTo(0, 0); 
 }
 
 function draw() {  
-	c.clearRect(0, 0, canvas.width, canvas.height); 
+  c.clearRect(0, 0, canvas.width, canvas.height); 
 
-	if(true || touchable) {	
+  if(true || touchable) {  
     var leftSet = false, rightSet = false;
 
-		for(var i = 0; i < touches.length; i++) {
-			var touch = touches[i]; 
+    for(var i = 0; i < touches.length; i++) {
+      var touch = touches[i]; 
 
-      var left = (touch.clientX < window.innerWidth / 2);
-			c.beginPath(); 
-			c.fillStyle = 'white';
-			/*c.fillText('touch id : ' + touch.identifier 
+      var leftSide = (touch.clientX < window.innerWidth / 2);
+      c.beginPath(); 
+      c.fillStyle = 'white';
+      /*c.fillText('touch id : ' + touch.identifier 
         + ' x:' + touch.clientX
         + ' y:' + touch.clientY, 
         touch.clientX + 30, touch.clientY - 30
       );*/ 
-			c.beginPath(); 
-			c.strokeStyle = left? 'cyan': 'magenta';
-			c.lineWidth = '6';
-			c.arc(touch.clientX, touch.clientY, 40, 0, Math.PI * 2, true); 
-			c.stroke();
+      c.beginPath(); 
+      c.strokeStyle = leftSide? 'cyan': 'magenta';
+      c.lineWidth = '6';
+      c.arc(touch.clientX, touch.clientY, 40, 0, Math.PI * 2, true); 
+      c.stroke();
 
-      if(left) {
+      if(leftSide) {
         powerL = ((window.innerHeight / 2) - touch.clientY) / (window.innerHeight / 2);
         leftSet = true;
       } else {
         powerR = ((window.innerHeight / 2) - touch.clientY) / (window.innerHeight / 2);
         rightSet = true;
       }
-		}
+    }
     if(!leftSet) {
       powerL = 0;
     }
@@ -68,45 +68,55 @@ function draw() {
       powerR = 0;
     }
 
-		c.fillStyle = 'white';
-		c.fillText('left: ' + powerL, 30, window.innerHeight - 20); 
-		c.fillText('right: ' + powerR, window.innerWidth / 2 + 30, window.innerHeight - 20); 
-	  c.fillText('rate: ' + updateRate + ' Hz', 10, 15);
+    c.fillStyle = 'white';
+    var powers = RobotControl.getPower();
+    c.fillText('left: ' + powers.left, 30, window.innerHeight - 20); 
+    c.fillText('right: ' + powers.right, window.innerWidth / 2 + 30, window.innerHeight - 20); 
+    c.fillText('rate: ' + RobotControl.getUpdateRate() + ' Hz', 10, 15);
 
-	} else {		
-		c.fillStyle	 = 'white'; 
-		c.fillText('mouse : ' + mouseX + ', ' + mouseY, mouseX, mouseY); 
-	}	
+    var state = RobotControl.getState();
+    c.fillStyle = (state === RobotControl.ERROR)? 'red': 'green';
+    c.fillText('state: ' + state.toUpperCase(), 80, 15);
+
+  } else {    
+    c.fillStyle   = 'white'; 
+    c.fillText('mouse : ' + mouseX + ', ' + mouseY, mouseX, mouseY); 
+  }
+  
+  var sensitivity = 2;
+  var left  = Math.floor(powerL * 1023 * sensitivity);
+  var right = Math.floor(powerR * 1023 * sensitivity);
+  RobotControl.setPower(left, right);
 }
 
-/*	
- *	Touch event (e) properties : 
- *	e.touches: 			Array of touch objects for every finger currently touching the screen
- *	e.targetTouches: 	Array of touch objects for every finger touching the screen that
- *						originally touched down on the DOM object the transmitted the event.
- *	e.changedTouches	Array of touch objects for touches that are changed for this event. 					
- *						I'm not sure if this would ever be a list of more than one, but would 
- *						be bad to assume. 
+/*  
+ *  Touch event (e) properties : 
+ *  e.touches:       Array of touch objects for every finger currently touching the screen
+ *  e.targetTouches:   Array of touch objects for every finger touching the screen that
+ *            originally touched down on the DOM object the transmitted the event.
+ *  e.changedTouches  Array of touch objects for touches that are changed for this event.           
+ *            I'm not sure if this would ever be a list of more than one, but would 
+ *            be bad to assume. 
  *
- *	Touch objects : 
+ *  Touch objects : 
  *
- *	identifier: An identifying number, unique to each touch event
- *	target: DOM object that broadcast the event
- *	clientX: X coordinate of touch relative to the viewport (excludes scroll offset)
- *	clientY: Y coordinate of touch relative to the viewport (excludes scroll offset)
- *	screenX: Relative to the screen
- *	screenY: Relative to the screen
- *	pageX: Relative to the full page (includes scrolling)
- *	pageY: Relative to the full page (includes scrolling)
- */	
+ *  identifier: An identifying number, unique to each touch event
+ *  target: DOM object that broadcast the event
+ *  clientX: X coordinate of touch relative to the viewport (excludes scroll offset)
+ *  clientY: Y coordinate of touch relative to the viewport (excludes scroll offset)
+ *  screenX: Relative to the screen
+ *  screenY: Relative to the screen
+ *  pageX: Relative to the full page (includes scrolling)
+ *  pageY: Relative to the full page (includes scrolling)
+ */  
 function onTouchStart(e) { 
-	touches = e.touches; 
+  touches = e.touches; 
 }
  
 function onTouchMove(e) {
-	// Prevent the browser from doing its default thing (scroll, zoom)
-	e.preventDefault();
-	touches = e.touches; 
+  // Prevent the browser from doing its default thing (scroll, zoom)
+  e.preventDefault();
+  touches = e.touches; 
 } 
  
 function onTouchEnd(e) { 
@@ -114,54 +124,22 @@ function onTouchEnd(e) {
 }
 
 function onMouseMove(event) {
-	mouseX = event.offsetX;
-	mouseY = event.offsetY;
+  mouseX = event.offsetX;
+  mouseY = event.offsetY;
 }
 
-function setupCanvas() {	
-	canvas = document.createElement('canvas');
-	c = canvas.getContext( '2d' );
+function setupCanvas() {  
+  canvas = document.createElement('canvas');
+  c = canvas.getContext( '2d' );
 
-	container = document.createElement('div');
-	container.className = 'container';
-	canvas.width  = window.innerWidth; 
-	canvas.height = window.innerHeight; 
+  container = document.createElement('div');
+  container.className = 'container';
+  canvas.width  = window.innerWidth; 
+  canvas.height = window.innerHeight; 
 
-	document.body.appendChild(container);
-	container.appendChild(canvas);	
-	
-	c.strokeStyle = '#ffffff';
-	c.lineWidth   = 2;	
-}
-
-var lastTime = new Date();
-
-function sendUpdate() {
-  var xhr = new XMLHttpRequest();
+  document.body.appendChild(container);
+  container.appendChild(canvas);  
   
-  xhr.open('PUT', '/control', true);
-  xhr.onreadystatechange = function() {
-    if(xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
-      //console.log(xhr.responseText);
-      setTimeout(sendUpdate, 1);
-      
-      var delayMs = new Date().getTime() - lastTime.getTime();
-      if(delayMs > 0) {
-        updateRate = Math.floor(1000 * 100 / delayMs) / 100;
-      } 
-    }
-  };
-  
-  var sensitivity = 2;
-  var left  = Math.floor(powerL * 1023 * sensitivity);
-  if(left > 1023) left = 1023;
-  if(left < -1023) left = -1023;
-
-  var right = Math.floor(powerR * 1023 * sensitivity);
-  if(right > 1023) right = 1023;
-  if(right < -1023) right = -1023;
-
-  //console.log('sending: ', left, right);
-  xhr.send(left + ':' + right);
-  lastTime = new Date();
+  c.strokeStyle = '#ffffff';
+  c.lineWidth   = 2;  
 }
