8cc2bc01d555ffbd0109c134c90150e170d5f164
Jeff Malins <jmalins@iolani.org>
2017-05-28 18:40:31 -0700

    added linting and filesize check to build

--- client/rollup.config.js
+++ client/rollup.config.js
@@ -1,13 +1,17 @@
 import buble from 'rollup-plugin-buble'
 import cleanup from 'rollup-plugin-cleanup'
+import eslint from 'rollup-plugin-eslint'
+import filesize from 'rollup-plugin-filesize';
 
 export default {
   entry: 'lib/index.js',
   format: 'cjs',
   treeshake: false,
   plugins: [
+    eslint(),
     buble({ objectAssign: 'Object.assign' }),
-    cleanup()
+    cleanup(),
+    filesize()
   ],
   dest: 'build/bundle.js',
   sourceMap: true,
