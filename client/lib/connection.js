f697b1b71e6026b87344eb930925e557f8b88cd3
Jeff Malins <jmalins@iolani.org>
2017-05-29 17:23:49 -0700

    implemented coonection classes

--- client/lib/connection.js
+++ client/lib/connection.js
@@ -5,109 +5,213 @@
  * @module connection-base
  */
 
-/* global XMLHttpRequest, WebSocket */
+/* global WebSocket */
+import { ajaxPut } from './utils'
 
-export const CONNECTED = 'CONNECTED'
-export const DISCONNECTED = 'DISCONNECTED'
-export const TIMEOUT = 'TIMEOUT'
-
-/** Control connection abstract base class */
+/**
+ * Base interface for connection to robot. This hides the differences
+ * between the AJAX and WebSocket communication modes.
+ *
+ * Implements a buffered asynchronous connection. External sources should
+ * call setRobotData() whenever new data is available. It will be sent to the robot
+ * at the fasted rate the connection will allow.
+ */
 export class Connection {
   constructor () {
-    this.status = DISCONNECTED
+    this.state = Connection.DISCONNECTED
     this.enabled = false
-    this.leftPower = 0
-    this.rightPower = 0
+    // response information //
+    this.lastError = null
+    this.responseData = null
+    // event handlers //
+    this.onstatechange = null
+    this.onresponsedata = null
   }
 
-  setEnabled (state) {
-    this.enabled = state
+  start () {
+    this.enabled = true
   }
 
-  /**
-   * Set power to wheels.
-   *
-   * Values range from -1.0 (full reverse) to 1.0 (full forward). A value of 0
-   * is stopped.
-   *
-   * @param {number} left wheel power [-1.0, 1.0]
-   * @param {number} right wheel power [-1.0, 1.0]
-   */
-  setWheelPower (left, right) {
-    if (left < -1.0) left = -1.0
-    if (left > 1.0) left = 1.0
-    if (right < -1.0) right = -1.0
-    if (right > 1.0) right = 1.0
-
-    this.leftPower = left
-    this.rightPower = right
-    this.update()
+  stop () {
+    this.enabled = false
   }
 
   /**
-   * Set power/value to an auxially channel.
-   *
-   * These can be weapon ESCs, servos, or general PWM channels based
-   * on how the robot is configured.
-   *
-   * @param {number} channel number (0..n)
-   * @param {number} power [0, 1.0]
+   * Update the connection state and notify listeners.
    */
-  setAuxChannel (channel, power) {
+  setState (newState) {
+    if (newState === this.state) return
+    const oldState = this.state
+    this.state = newState
 
+    // notify listener //
+    if (typeof this.onstatechange === 'function') {
+      this.onstatechange(newState, oldState)
+    }
   }
 
-  // base method implemented by subclasses //
-  update () {
+  setRobotData (data) {
+    this.dataPacket = (typeof data === 'string') ? data : JSON.stringify(data)
+  }
 
+  getResponseData () {
+    return this.responseData
+  }
+
+  setResponseData (data) {
+    this.responseData = data
+
+    // notify listener //
+    if (typeof this.onresponsedata === 'function') {
+      this.onresponsedata(data)
+    }
   }
 }
+Connection.CONNECTED = 'Connected'
+Connection.CONNECTING = 'Connecting'
+Connection.DISCONNECTED = 'Disconnected'
+Connection.ERROR = 'Error'
 
+/**
+ * Robot connection implemented with repetitive AJAX PUTs. In
+ * practice, this method is limited to 10-15Hz, but is a lowest
+ * common denominator supported by nearly all devices.
+ */
 export class AjaxConnection extends Connection {
   /**
    * Constructor.
    *
    * @param {number} connection timeout in milliseconds
    */
-  constructor (timeoutMillis = 250) {
+  constructor (timeoutMillis) {
     super()
-    this.timeoutMillis = timeoutMillis
+    this.timeoutMillis = timeoutMillis || 250
+    this.timerId = null
   }
 
-  setEnabled (state) {
-    super.setEnabled(state)
-  }
-
-  updateState (event, data) {
-    console.log('updateState', event, data)
-  }
-
-  send (value) {
-    const xhr = new XMLHttpRequest()
-
-    xhr.timeout = this.timeoutMillis
-    xhr.open('PUT', '/control', true)
-    xhr.onload = (info) => {
-      const data = { text: xhr.responseText, status: xhr.status, info }
-      switch (xhr.status) {
-        case 200:
-          this.updateState('success', data)
-          break
-        default:
-          this.updateState('unknown', data)
+  /**
+   *  Poll the robot in a continuous loop.
+   */
+  poll () {
+    const pollStart = new Date()
+    ajaxPut('/control', this.dataPacket, this.timeoutMillis, (err, res) => {
+      // was the loop terminated? //
+      this.lastError = err
+      if (this.state === Connection.DISCONNECTED) {
+        this.updateRate = 0
+        return
       }
-    }
-    xhr.onerror = (info) => {
-      this.updateState('error', { text: 'an error occurred', info })
-    }
-    xhr.ontimeout = (info) => {
-      this.updateState('event', { text: 'connected timed out', info })
-    }
 
-    xhr.send(value)
+      // handle response //
+      if (!this.lastError) {
+        if (this.state !== Connection.CONNECTED) {
+          this.setState(Connection.CONNECTED)
+        }
+        this.setResponseData(res.data)
+      } else {
+        this.setState(Connection.ERROR)
+      }
+
+      // poll again //
+      const pollMs = (this.state === Connection.ERROR) ? 1000 : 1 // back off if error //
+      this.timerId = setTimeout(this.poll.bind(this), pollMs)
+
+      // compute update rate //
+      var delayMs = new Date().getTime() - pollStart.getTime()
+      if (delayMs > 0) {
+        this.updateRate = Math.floor(1000 / delayMs)
+      }
+    })
+  }
+
+  start () {
+    this.setState(Connection.CONNECTING)
+    super.start()
+    this.poll()
+  }
+
+  stop () {
+    if (this.timerId) clearTimeout(this.timerId)
+    super.stop()
+    this.setState(Connection.DISCONNECTED)
   }
 }
 
+/**
+ * Robot connection implemented with a WebSocket. This connection allows
+ * for higher update rates, but it requires a modern phone OS / browser.
+ */
 export class WebSocketConnection extends Connection {
+  /**
+   * Constructor.
+   *
+   * @param {number} connection timeout in milliseconds
+   */
+  constructor (hostName) {
+    super()
+    this.hostName = hostName || document.location.hostname
+    this.socket = null
+  }
 
+  start () {
+    this.setState(Connection.CONNECTING)
+    super.start()
+
+    this.socket = new WebSocket(`ws://${this.hostName}/ws`, [ 'arduino' ])
+    this.socket.onopen = () => {
+      this.setState(Connection.CONNECTED)
+    }
+    this.socket.onerror = (err) => {
+      this.lastError = err
+      this.setState(Connection.ERROR)
+    }
+    this.socket.onmessage = (event) => {
+      if (event.data === 'heartbeat') {
+        this.lastHeartbeat = new Date()
+      } else {
+        this.setResponseData(event.data)
+      }
+    }
+    this.socket.onclose = (event) => {
+      if (this.state !== Connection.ERROR) {
+        this.setState(Connection.DISCONNECTED)
+      }
+    }
+  }
+
+  stop () {
+    if (this.socket) {
+      if (this.socket.readyState === WebSocket.OPEN) {
+        this.socket.close()
+      }
+      this.socket = null
+    }
+    super.stop()
+    this.setState(Connection.DISCONNECTED)
+  }
+
+  setRobotData (data) {
+    super.setRobotData(data)
+
+    // send an update //
+    if (this.socket && this.state === Connection.CONNECTED) {
+      // verify WebSocket state //
+      if (this.socket.readyState !== WebSocket.OPEN) {
+        this.lastError = (`Invalid socket state: ${
+            (this.socket.readyState === WebSocket.CONNECTING) ? 'CONNECTING'
+          : (this.socket.readyState === WebSocket.CLOSING) ? 'CLOSING'
+          : (this.socket.readyState === WebSocket.CLOSED) ? 'CLOSED'
+          : 'UNKNOWN'
+        }`)
+        this.setState(Connection.ERROR)
+        return
+      }
+      try {
+        this.socket.send(this.dataPacket)
+      } catch (err) {
+        this.lastError = err
+        this.setState(Connection.ERROR)
+      }
+    }
+  }
 }
